f1fdfd9d84cf6e3d6c07b030e640348e10f3a988
Marvin T <contact@marvinkturner.com>
2021-01-11 16:36:25 -0500

    feat: add definition for Company class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { User } from './User' 
+import { Company } from "./Company";
 
+// test user
 const user = new User();
 console.log(user);
+
+// test company
+const company = new Company();
+console.log(company);
